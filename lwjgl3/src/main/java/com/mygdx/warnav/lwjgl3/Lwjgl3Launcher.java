0a0a6915176a1f4c6a2351de1e4f1c3e560a6093
Fernando <fernando.lopez.m01@mail.pucv.cl>
2025-10-29 17:49:40 -0300

    Se importo el juego base

--- lwjgl3/src/main/java/com/mygdx/warnav/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/com/mygdx/warnav/lwjgl3/Lwjgl3Launcher.java
@@ -2,7 +2,7 @@ package com.mygdx.warnav.lwjgl3;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.mygdx.warnav.WarNavGame;
+import com.mygdx.warnav.GameLluviaMenu;
 
 /** Launches the desktop (LWJGL3) application. */
 public class Lwjgl3Launcher {
@@ -12,7 +12,7 @@ public class Lwjgl3Launcher {
     }
 
     private static Lwjgl3Application createApplication() {
-        return new Lwjgl3Application(new WarNavGame(), getDefaultConfiguration());
+        return new Lwjgl3Application(new GameLluviaMenu(), getDefaultConfiguration());
     }
 
     private static Lwjgl3ApplicationConfiguration getDefaultConfiguration() {
